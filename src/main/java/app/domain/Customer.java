4e035812b57369d6912bdf24d6dff33710758e79
maximheit1989-arch <maxim.heit1989@gmail.com>
2025-08-29 12:04:27 +0200

    domain layer implemented

---
+++ src/main/java/app/domain/Customer.java
@@ -0,0 +1,34 @@
+package app.domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Customer {
+
+    private long id;
+    private String name;
+    private boolean active;
+    private List<Product> cart;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && active == customer.active && Objects.equals(name, customer.name) && Objects.equals(cart, customer.cart);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, active, cart);
+    }
+
+    @Override
+    public String toString() {
+        String info = String.format("Customer: id - %d, name - %s, active - %s%n", id, name, active ? "yes" : "no");
+        StringBuilder builder = new StringBuilder(info);
+        builder.append("Cart:");
+        cart.forEach(x -> builder.append(" ").append(x.getTitle()));
+        return builder.toString();
+
+    }
+}
---
+++ src/main/java/app/domain/Product.java
@@ -0,0 +1,36 @@
+package app.domain;
+
+import java.util.Objects;
+
+/*
+Этот класс находится на первом слое нашего приложения - домен.
+Домен содержит классы, описывающие сущности, с которыми работает приложение.
+ */
+public class Product {
+
+    private Long id;
+    private String title;
+    private double price;
+    private boolean active;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && active == product.active && Objects.equals(id, product.id) && Objects.equals(title, product.title);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price, active);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product: id - %d, title - %s, price - %.2f, active - %s", id, title, price, active ? "yes" : "no");
+    }
+}
